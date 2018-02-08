481a57a8207320533db1c8f069aa21c110b4436c
Patrick <bleowy1@gmail.com>
2018-02-09 00:00:56 +0100

    Initial commit

---
+++ desktop/src/pl/bbl/osbir/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.bbl.osbir.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.bbl.osbir.AnimationEditor;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AnimationEditor(), config);
+	}
+}
