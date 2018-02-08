b190f659d5407264348ba50e3ee03fbf02fcbfc5
Patrick <bleowy1@gmail.com>
2018-02-09 00:35:16 +0100

    Added OsbirClient.java to get access to AnimationClasses.

--- core/src/pl/bbl/osbir/AnimationEditor.java
+++ core/src/pl/bbl/osbir/AnimationEditor.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import pl.bbl.osbir.engine.animations.AnimationData;
 
 public class AnimationEditor extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -14,6 +15,7 @@ public class AnimationEditor extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
 	}
 
 	@Override
